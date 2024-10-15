fa9587e3cb34e568a2fe6a085ad630a1828087d1
aryaashari <aryaashari100@gmail.com>
2024-10-16 04:20:51 +0700

    install laravel breeze react inertia

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
+use App\Http\Controllers\Frontend\DocumentsFrontendController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-use App\Http\Controllers\Frontend\DocumentsFrontendController;
 
 Route::get('/', [DocumentsFrontendController::class, "showSubmit"]);
 Route::get('/daftar', [DocumentsFrontendController::class, "showList"]);
 
-Route::get('/inertiatest', function () {
-    return Inertia::render('TestDemo/TestDemo', [
-        'date' => date("Y-m-d"),
-    ]); // This will get default component from the resources/js/pages/TestDemo/TestDemo.jsx
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
