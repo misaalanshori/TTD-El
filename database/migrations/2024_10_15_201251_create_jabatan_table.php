b8a1b3a039287f5675bdf70f1c2f1dc1bd178b0e
aryaashari <aryaashari100@gmail.com>
2024-10-24 21:28:42 +0700

    change key type for id primary key users table

--- database/migrations/2024_10_15_201251_create_jabatan_table.php
+++ database/migrations/2024_10_15_201251_create_jabatan_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('jabatan', function (Blueprint $table) {
             $table->id();
-            $table->uuid('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('jabatan');
             $table->string('nip');
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
