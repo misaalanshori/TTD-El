9b2b0039430c47246c2c00b00c334079c04b4e8d
aryaashari <aryaashari100@gmail.com>
2024-10-29 07:58:47 +0700

    crud jabatan

---
+++ app/Http/Controllers/JabatanController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Jabatan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class JabatanController extends Controller
+{
+    
+    public function index() {
+
+        $jabatan = Jabatan::with('user')->get();
+        return "ok";
+    }
+
+    public function store(Request $request) {
+
+        $request->validate([
+            'jabatan' => 'required',
+            'nip' => 'required|integer'
+        ]);
+
+        $user = Auth::user();
+        Jabatan::create([
+            'user_id' => $user->id,
+            'jabatan' => $request->jabatan,
+            'nip' => $request->nip
+        ]);
+
+        return 'ok';
+
+    } 
+
+    public function update(Request $request, $id) {
+
+        $request->validate([
+            'jabatan' => 'required',
+            'nip' => 'required|integer'
+        ]);
+
+        $jabatan = Jabatan::findOrFail($id);
+        $jabatan->update([
+            'jabatan' => $request->jabatan,
+            'nip' => $request->nip
+        ]);
+
+        return 'ok';
+
+    }
+
+    public function destroy($id) {
+        Jabatan::destroy($id);
+        return 'ok';
+    } 
+
+}
