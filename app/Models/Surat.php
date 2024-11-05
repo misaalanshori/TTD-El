66279b8a413241e151b4009e1b8d640ffa8cd9ff
M Isa Al Anshori <isabungsu@gmail.com>
2024-11-05 13:14:45 +0700

    add id to pivot

--- app/Models/Surat.php
+++ app/Models/Surat.php
@@ -18,6 +18,6 @@ class Surat extends Model
     protected $guarded = [];
 
     public function jabatan() {
-        return $this->belongsToMany(Jabatan::class, 'surat_pengguna', 'surat_id', 'jabatan_id')->withPivot(['qrcode_file']);
+        return $this->belongsToMany(Jabatan::class, 'surat_pengguna', 'surat_id', 'jabatan_id')->withPivot(['id', 'qrcode_file']);
     }
 }
