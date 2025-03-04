0ccb2e244deaddc41fa82c5f715bd05acdbd0001
M Isa Al Anshori <isabungsu@gmail.com>
2025-03-04 20:11:15 +0700

    Update Document Submit Page

--- resources/js/Pages/Documents/SubmitDocument.jsx
+++ resources/js/Pages/Documents/SubmitDocument.jsx
@@ -1,16 +1,17 @@
-import { Autocomplete, Avatar, Button, Collapse, Fade, List, ListItem, ListItemAvatar, ListItemButton, ListItemText, Stack, TextField, Typography } from "@mui/material";
+import { Autocomplete, Avatar, Button, Checkbox, Collapse, Fade, FormControlLabel, FormGroup, List, ListItem, ListItemAvatar, ListItemButton, ListItemText, Stack, TextField, Typography } from "@mui/material";
 import MainLayout from "@/Layouts/MainLayout/MainLayout";
 import { Add, ArrowForward, Clear, TurnedInNot } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import { TransitionGroup } from "react-transition-group";
 import UploadCard from "./partials/UploadCard";
 import { useSnackbar } from "notistack";
-import { Head, router } from "@inertiajs/react";
+import { Head, router, usePage } from "@inertiajs/react";
 
 
 
 export default function SubmitDocument({ users, kategori }) {
-    const { enqueueSnackbar } = useSnackbar()
+    const auth = usePage().props.auth;
+    const { enqueueSnackbar } = useSnackbar();
     const [selectedDocument, setSelectedDocument] = useState(null);
     const [formData, setFormdata] = useState(null)
     const [selectedUser, setSelectedUser] = useState(null);
@@ -20,6 +21,9 @@ export default function SubmitDocument({ users, kategori }) {
     const [signers, setSigners] = useState([]);
     const [errors, setErrors] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [personalSubmission, setPersonalSubmission] = useState(false);
+
+    const isSelfSigning = selectedUser?.id === auth.user.id || signers.some(v => v.data.user_id === auth.user.id);
 
     const handleAddSigner = () => {
         if (!selectedJabatan) return;
@@ -38,18 +42,24 @@ export default function SubmitDocument({ users, kategori }) {
     const handleUpdateSelectedUser = async (e, v) => {
         setAvailableJabatan(null)
         setSelectedUser(v);
-        if (v) {
-            const response = await fetch(route("getJabatanByUserId", { id: v.id }))
-            if (response.status == 200) {
-                const json = await response.json()
-                setAvailableJabatan(json.map(j => ({ id: j.id, label: j.jabatan, data: j })))
-            }
-
-        } else {
-            setAvailableJabatan(null)
-        }
-        setSelectedJabatan(null)
     }
+
+    useEffect(() => {
+        const updateAvailableJabatan = async () => {
+            if (selectedUser) {
+                const response = await fetch(route("getJabatanByUserId", { id: selectedUser.id }))
+                if (response.status == 200) {
+                    const json = await response.json()
+                    setAvailableJabatan(json.map(j => ({ id: j.id, label: j.jabatan, data: j })))
+                }
+
+            } else {
+                setAvailableJabatan(null)
+            }
+            setSelectedJabatan(null)
+        }
+        updateAvailableJabatan();
+    }, [selectedUser])
 
     const handleRemoveSigner = (id) => {
         setSigners(signers.filter(v => v.id != id));
@@ -89,7 +99,26 @@ export default function SubmitDocument({ users, kategori }) {
                 setLoading(false);
             }
         }
-        router.post(route("createDocument", {continue_sign}), collectFormData(), callbacks);
+        router.post(route("createDocument", { continue_sign }), collectFormData(), callbacks);
+    }
+
+    const handlePersonalSubmissionChange = (e) => {
+        setPersonalSubmission(e.target.checked);
+        if (e.target.checked) {
+            setFormdata({
+                ...formData,
+                pengaju: auth.user.name,
+            })
+        } else {
+            setFormdata({
+                ...formData,
+                pengaju: "",
+            })
+        }
+    }
+
+    const handleAddSelfSigner = () => {
+        setSelectedUser(users.find(v => v.id === auth.user.id));
     }
 
     const resetForm = () => {
@@ -115,12 +144,17 @@ export default function SubmitDocument({ users, kategori }) {
                 <UploadCard document={selectedDocument} onDocumentChanged={setSelectedDocument} />
                 <Fade in={!!selectedDocument} unmountOnExit>
                     <Stack sx={{ width: "85%", maxWidth: 600, justifyContent: "center", alignItems: "center" }} gap={2}>
+                        <Stack sx={{ width: "100%", alignItems: "center" }}>
+                            <TextField fullWidth disabled={personalSubmission} error={!!errors?.pengaju} helperText={errors?.pengaju} value={formData?.pengaju || ""} name="pengaju" onChange={handleUpdateForm} label="Nama Pengaju" />
+                            <FormGroup sx={{ alignSelf: "flex-start" }}>
+                                <FormControlLabel control={<Checkbox checked={personalSubmission} onChange={handlePersonalSubmissionChange} />} label="Pengajuan pribadi" />
+                            </FormGroup>
+                        </Stack>
                         <Stack sx={{ width: "100%", alignItems: "center" }} gap={1}>
                             <Typography variant="h5" sx={{ fontWeight: "500" }}>Detail Dokumen</Typography>
-                            <TextField fullWidth error={!!errors?.pengaju} helperText={errors?.pengaju} value={formData?.pengaju || ""} name="pengaju" onChange={handleUpdateForm} label="Nama Pengaju" />
                             <TextField fullWidth error={!!errors?.judul_surat} helperText={errors?.judul_surat} value={formData?.judul_surat || ""} name="judul_surat" onChange={handleUpdateForm} label="Judul Dokumen" />
                             <TextField fullWidth error={!!errors?.nomor_surat} helperText={errors?.nomor_surat} value={formData?.nomor_surat || ""} name="nomor_surat" onChange={handleUpdateForm} label="Nomor Surat" />
-                            <TextField fullWidth error={!!errors?.keterangan} helperText={errors?.keterangan} value={formData?.keterangan || ""} name="keterangan" onChange={handleUpdateForm} multiline label="Keterangan" />
+                            <TextField fullWidth error={!!errors?.keterangan} helperText={errors?.keterangan} value={formData?.keterangan || ""} name="keterangan" onChange={handleUpdateForm} multiline label="Deskripsi" />
                             <Autocomplete
                                 fullWidth
                                 disablePortal
@@ -144,11 +178,10 @@ export default function SubmitDocument({ users, kategori }) {
                                                 <ListItemButton sx={{ flexGrow: 0 }} onClick={() => handleRemoveSigner(v.id)}><Clear /></ListItemButton>
                                             </ListItem>
                                         </Collapse>
-
                                     )}
                                 </TransitionGroup>
-
                             </List>
+                            {isSelfSigning ? null : <Button variant="outlined" size="small" startIcon={<Add />} onClick={handleAddSelfSigner}>Tambahkan saya sebagai penandatangan</Button>}
                             <Stack sx={{ width: "100%", alignItems: "center", flexDirection: { xs: "column", md: "row" } }} gap={1}>
                                 <Stack sx={{ width: "100%" }} gap={1}>
                                     <Autocomplete
